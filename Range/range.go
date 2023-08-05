831f7c0d0fb7313855e4e307a70c9ea8f0dc4bc3
JD <davermn5@gmail.com>
2023-08-04 20:51:25 -0700

    Range, Updated comments.

--- Range/range.go
+++ Range/range.go
@@ -7,14 +7,14 @@ func main() {
 	//Here we use range to sum the numbers in a slice. Arrays work like this too.
 	nums := []int{2, 3, 4}
 	sum := 0
-	for _, num := range nums {
+	for _, num := range nums { //Notice that the '_' is present, which indicates the intent to use the value (but not the key)..
 		sum += num
 	}
 	fmt.Println("sum:", sum)
 
 	// i represents the index, starting at 0
 	// num represents the value of the index
-
+	//Notice that the '_' (blank identifier is absent); This signals an intent to use both key and value.
 	for i, num := range nums {
 		if num == 3 {
 			fmt.Println("index:", i)
